537af0e45a2d1ad8e40077e17d1dc4c950ac49f9
Tomas Unzain <tomiunzv@gmail.com>
2023-04-03 20:46:19 -0400

    mostrar datos categorias en tabla

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { CategoryService } from 'src/app/modules/shared/services/category.service';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit {
+  constructor(private categoryService: CategoryService){
+  }
 
+  ngOnInit(): void{
+    this.getCategories();
+  }
+
+  displayedColums: string[]=['id','name','description','actions'];
+  dataSourse = new MatTableDataSource<CategoryElement>();
+
+  getCategories(){
+    this.categoryService.getCategories()
+      .subscribe( (data:any)=>{
+        console.log("respuesta categories: ", data);
+        this.processCategoriesResponse(data);
+      }, (error: any)=>{
+        console.log("Error: ",error);
+      })
+  }
+
+  processCategoriesResponse(resp: any){
+    const dataCategory: CategoryElement[] = [];
+    if (resp.metadata[0].code == "00") {
+      let listCategory = resp.categoryResponse.category;
+      listCategory.forEach((element: CategoryElement) => {
+        dataCategory.push(element)
+      });
+      this.dataSourse = new MatTableDataSource<CategoryElement>(dataCategory);
+    }
+  }
+}
+
+export interface CategoryElement {
+  id: number;
+  name: string;
+  description: string;
 }
