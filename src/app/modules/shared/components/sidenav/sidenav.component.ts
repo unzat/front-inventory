113fbb9a7450c395af935352ec41b06a14569683
Tomas Unzain <tomiunzv@gmail.com>
2023-04-16 17:38:00 -0400

    integracion keycloack

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,13 +1,15 @@
 import { MediaMatcher } from '@angular/cdk/layout';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.css']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit{
   mobileQuery: MediaQueryList;
+  username: string="";
 
   menuNav = [
     {name: "Home", route: "home", icon: "home"},
@@ -15,7 +17,15 @@ export class SidenavComponent {
     {name: "Productos", route: "product", icon: "production_quantity_limits"},
   ]
 
-  constructor(media: MediaMatcher){
+  constructor(media: MediaMatcher, private keycloakService: KeycloakService){
     this.mobileQuery = media.matchMedia('(max-width: 600px)');
+  }
+  ngOnInit(): void {
+    this.username = this.keycloakService.getUsername();
+    this.username = this.username.toUpperCase();
+  }
+
+  logout() {
+    this.keycloakService.logout();
   }
 }
